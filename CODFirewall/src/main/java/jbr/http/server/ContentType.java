430f57151594edda1aec0afd5b7e182151a12c9f
José Ángel <angel.belda@altia.es>
2020-10-15 13:52:07 +0200

    *WARNING* jar does not execute properly

--- CODFirewall/src/main/java/jbr/http/server/ContentType.java
+++ CODFirewall/src/main/java/jbr/http/server/ContentType.java
@@ -1,29 +1,31 @@
 package jbr.http.server;
 
 public enum ContentType {
-  
+
   HTML("text/html"),
-  
+
   CSS("text/css"),
-  
+
   JAVASCRIPT("text/javascript"),
-  
+
   PNG("image/png"),
-  
+
   JPEG("image/jpeg"),
-  
+
   GIF("image/gif"),
-  
+
   BMP("image/bmp"),
-  
+
+  JSON("application/json"),
+
   PLAIN("text/plain");
-  
+
   private String value;
 
   private ContentType(String value) {
     this.value = value;
   }
-  
+
   @Override
   public String toString() {
     return value;
